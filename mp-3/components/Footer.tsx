d0817183a906ec40237272a3122754ba81cf24be
szetol0718 <szetol0718@icloud.com>
2025-03-08 05:17:56 +0800

    components made

---
+++ mp-3/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>All Rights Reserved by Yat Long Szeto (Louis) &copy;</p>
+        </footer>
+    );
+};
+export default Footer;
---
+++ mp-3/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Yat Long Szeto (Louis)</h1>
+            <p>My Online Resume</p>
+        </header>
+    );
+};
+
+export default Header;
